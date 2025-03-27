b2713f85c6a3aa0e5ebb81f0ccb48e5727e64a90
Arnav Pawar <109753432+Arnav1333@users.noreply.github.com>
2025-03-28 00:14:28 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-    let element = document.body;
-    element.classList.toggle("dark");
-}
-function toggleMenu() {
-    let nav = document.querySelector('.nav-link');
-    nav.classList.toggle('active');
-}
